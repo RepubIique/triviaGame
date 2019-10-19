395df646d5c367c6a9f637fe9de8927658a4611b
RepubIique <kendrickbong1996@gmail.com>
2019-10-20 02:37:43 +0800

    submit button to be fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
+//forced global variables
+
+let correctAnswer = [];
+let incorrectAnswers = [];
+
 //~~~~~~~~~~~~~~~~~~~~~~ GameContainer logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 function displayQuestions() {
@@ -10,10 +15,9 @@ function displayQuestions() {
     method: "GET"
   }).then(function(response) {
     for (let i = 0; i < response.results.length; i++) {
-      let answers = [
-        response.results[i].correct_answer,
-        ...response.results[i].incorrect_answers
-      ];
+      correctAnswer = response.results[i].correct_answer;
+      incorrectAnswers = response.results[i].incorrect_answers;
+      let answers = [correctAnswer, ...incorrectAnswers];
       $("#question").html("<h2>" + response.results[i].question + "</h2>");
 
       $("#answers").append(
@@ -34,38 +38,51 @@ function displayQuestions() {
           answers[3] +
           "</label><br>"
       );
-
-      //~~~~~~~~~~~~~~~~~~~~~~ Reset logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-      function reset() {
-        $("#question").html("");
-        $("#answers").html("");
-        displayQuestions();
-      }
-      //~~~~~~~~~~~~~~~~~~~~~~ Timer logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-      var number = 40;
-      var intervalId;
-
-      function run() {
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000);
-      }
-
-      function decrement() {
-        number--;
-        $("#show-number").html("<h3>" + "Time left: " + number + "</h3>");
-        if (number === 0) {
-          stop();
-          reset();
-        }
-      }
-
-      function stop() {
-        clearInterval(intervalId);
-      }
-
-      run();
     }
+
+    //~~~~~~~~~~~~~~~~~~~~~~ Reset logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    function reset() {
+      $("#question").html("");
+      $("#answers").html("");
+      displayQuestions();
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~ Timer logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    var number = 41;
+    var intervalId;
+
+    function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+      number--;
+      $("#show-number").html("<h3>" + "Time left: " + number + "</h3>");
+      if (number === 0) {
+        stop();
+        reset();
+      }
+    }
+
+    function stop() {
+      clearInterval(intervalId);
+    }
+
+    run();
   });
 }
 
 displayQuestions();
+
+//~~~~~~~~~~~ Submit Correct/Incorrect Answer logic ~~~~~~~~~~~~~~~//
+
+function answerChecker() {
+  if ((correctAnswer = true)) {
+    console.log("right");
+  } else {
+    console.log("nope");
+  }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~ End game after 3 rounds logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// to do
