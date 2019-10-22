cfc51a99544d6b788b093653cb5c850938b8ebc0
RepubIique <kendrickbong1996@gmail.com>
2019-10-22 18:24:16 +0800

    finished logic and added basic css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,89 +1,92 @@
 //Global variables
 
+let questionsAnswered = 0;
 let correctAnswer = [];
 let incorrectAnswers = [];
-let answers = [];
+let answersshuffle = [];
 let correctScore = 0;
 let incorrectScore = 0;
-//~~~~~~~~~~~~~~~~~~~~~~ GameContainer logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-
-function displayQuestions() {
-  const queryURL =
-    "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple";
-
-  //~~~~~~~~~~~~~~~~~~~~~~ Trivia Game API logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    for (let i = 0; i < response.results.length; i++) {
-      correctAnswer = response.results[i].correct_answer;
-      incorrectAnswers = response.results[i].incorrect_answers;
-      answers = [correctAnswer, ...incorrectAnswers];
-      $("#question").html("<h2>" + response.results[i].question + "</h2>");
-
-      $("#answers").append(
-        '<form name="multipleC">' +
-          '<input type="radio" value="r1" name="answer">' +
-          "<label>" +
-          answers[0] +
-          "</label><br>" +
-          '<input type="radio" value="r2" name="answer">' +
-          "<label>" +
-          answers[1] +
-          "</label><br>" +
-          '<input type="radio" value="r3" name="answer">' +
-          "<label>" +
-          answers[2] +
-          "</label><br>" +
-          '<input type="radio" value="r4" name="answer">' +
-          "<label>" +
-          answers[3] +
-          "</label><br>" +
-          "</form>"
-      );
-    }
-
-    //~~~~~~~~~~~~~~~~~~~~~~ Timer logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-    var number = 41;
-    var intervalId;
-
-    function run() {
-      clearInterval(intervalId);
-      intervalId = setInterval(decrement, 1000);
-    }
-
-    function decrement() {
-      number--;
-      $("#timer").html("<h3>" + "Time left: " + number + "</h3>");
-      if (number === 0) {
-        stop();
-        reset();
-      }
-    }
-
-    function stop() {
-      clearInterval(intervalId);
-    }
-
-    run();
-  });
-}
+let secondsLeft = 0;
+var downloadTimer = null;
 
 displayQuestions();
+//~~~~~~~~~~~~~~~~~~~~~~ GameContainer logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+function displayQuestions() {
+  // How many rounds do you want players to continue for?
+  if (questionsAnswered == 10) {
+    showResults();
+  } else {
+    $("#answers").html("");
 
-//~~~~~~~~~~~~~~~~~~~~~~ Reset logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-function reset() {
-  $("#question").html("");
-  $("#answers").html("");
-  $("#timer").html("");
-  displayQuestions();
+    const queryURL =
+      "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple";
+
+    //~~~~~~~~~~~~~~~~~~~~~~ Trivia Game API logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      for (let i = 0; i < response.results.length; i++) {
+        correctAnswer = response.results[i].correct_answer;
+        incorrectAnswers = response.results[i].incorrect_answers;
+        options = shuffle([correctAnswer, ...incorrectAnswers]);
+        $("#question").html("<h2>" + response.results[i].question + "</h2>");
+
+        $("#answers").append(`
+      <form name="multipleC">
+      <input type="radio" value="${options[0]}" name="answer">
+      <label>${options[0]}</label><br>
+      <input type="radio" value="${options[1]}" name="answer">
+      <label>${options[1]}</label><br>
+      <input type="radio" value="${options[2]}" name="answer">
+      <label>${options[2]}</label><br>
+      <input type="radio" value="${options[3]}" name="answer"> 
+      <label>${options[3]}</label><br>
+     `);
+      }
+      timerCountdown(20);
+    });
+  }
+}
+
+// Fisher-Algorithm for shuffling
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle…
+  while (0 !== currentIndex) {
+    // Pick a remaining element…
+    randomIndex = Math.floor(Math.random() * currentIndex--);
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~ Timer Countdown ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+function timerCountdown(durationInSeconds) {
+  let timeLeft = durationInSeconds;
+  downloadTimer = setInterval(function() {
+    $("#timer").html(`<h3>Time left:${timeLeft}</h3>`);
+    timeLeft -= 1;
+    if (timeLeft <= 0) {
+      clearInterval(downloadTimer);
+      showResults();
+      $("#timer").html(`<h3>Time has ran out</h3 >`);
+    }
+  }, 1000);
 }
 
 //~~~~~~~~~~~ Check Answer logic ~~~~~~~~~~~~~~~//
 
 function answerChecker(ans) {
-  var radios = multipleC.elements[ans];
+  questionsAnswered++;
+  let radios = multipleC.elements[ans];
   window.rdValue;
   for (var i = 0; i < radios.length; i++) {
     var someRadio = radios[i];
@@ -92,28 +95,33 @@ function answerChecker(ans) {
       break;
     } else rdValue = "noRadioChecked";
   }
-  if (rdValue == "r1") {
+
+  if (correctAnswer === rdValue) {
     answerIsCorrect();
-    reset();
-  } else if ((rdValue == "r2", "r3", "r4")) {
-    answerIsIncorrect();
-    reset();
   } else {
-    // not working
-    console.log("no radio checked");
+    answerIsIncorrect();
   }
 }
 
-//~~~~~~~~~~~~~~~~~~~~~~ End game after 3 rounds logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-// to do
-
-//~~~~~~~~~~~~~~~~~~~~~~ Win/Lose logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 function answerIsCorrect() {
   correctScore++;
-  $("#score-counter").html("<h3>" + "Score: " + correctScore + "</h3>");
+  $("#score-counter").html(`<h3>Score:${correctScore}</h3>`);
+  displayQuestions();
+  clearInterval(downloadTimer);
 }
 
 function answerIsIncorrect() {
   incorrectScore++;
-  $("#score-decounter").html("<h3>" + "Wrong: " + incorrectScore + "</h3>");
+  $("#score-decounter").html(`<h3>Wrong:${incorrectScore}</h3>`);
+  displayQuestions();
+  clearInterval(downloadTimer);
+}
+
+function showResults() {
+  $("#question").html("Game has ended");
+  $("#answers").html("");
+  $("#timer").html("");
+  $("#submitButton").hide();
+  $("#score-counter").html(`<h3>Score:${correctScore}</h3>`);
+  $("#score-decounter").html(`<h3>Wrong:${incorrectScore}</h3>`);
 }
